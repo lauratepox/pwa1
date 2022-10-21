fd8e4dc83da5ea7fb313b462db2910850bd96e0b
unknown <utp0143399@alumno.utpuebla.edu.mx>
2022-10-20 19:03:42 -0500

    pwa1

---
+++ sw.js
@@ -0,0 +1,41 @@
+var nombreCache='uno';
+
+self.addEventListener(
+    'install',
+    function(event){
+        // esperar hasta que la promesa que recibe como argumento sea concluida
+        event.waitUntil(
+            caches.open(nombreCache)
+            .then(
+                function(cache){
+                    cache.addAll(
+                        [
+                            'fig.js',
+                            'w3.css'
+                        ]
+                    );        
+                }
+            )
+        );    
+    }
+);
+
+self.addEventListener(
+    'fetch', 
+    function(event) {
+        event.respondWith( 
+            caches.match(event.request)
+            .then(
+                function(respuesta){
+                    if(respuesta){
+                        console.log('entró');
+                        return respuesta;
+                    }   
+                    else{
+                        return fetch(event.request);
+                    }  
+                }
+            ) 
+        );
+    }
+); 
